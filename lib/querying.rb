5ecd13981903df9574c8119ec049595c2cf6604f
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-02 02:51:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,5 +29,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.names, COUNT(character_books.book_id) FROM books
+  INNER JOIN characters.id = character_books.character_id
+  GROUP BY characters.names; "
 end
