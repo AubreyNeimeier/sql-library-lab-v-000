73180ba888f506706e2293a3da253ae8932c648d
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-02 02:29:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,7 @@ def select_series_title_with_most_human_characters
   INNER JOIN characters ON series.id = characters.series_id
   WHERE characters.species = 'human'
   GROUP BY series.title
-  ORDER BY COUNT(characters.species) LIMIT 1;"
+  ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
