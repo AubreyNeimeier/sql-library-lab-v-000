ccc023f93072092b418ae87cdf55e0a719e9918c
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-02 02:27:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,8 +23,9 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.titles FROM series
   INNER JOIN characters ON series.id = characters.series_id
-  WHERE character.species = 'human' 
-  GROUP BY series.title "
+  WHERE character.species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(character.species) LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
